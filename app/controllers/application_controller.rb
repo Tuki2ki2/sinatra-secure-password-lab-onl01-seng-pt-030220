907c67598b71405c2d1f41c657effdd01e7a5d39
github username <github email address>
2020-06-07 18:01:53 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-  user = User.new(name:params[:name], password:params[:password])
-  if user.save 
-    redirect "/login"
-  else
-    redirect "/failure"
+    if params[:username] == "" || params[:password] == ""
+    redirect "/failure" 
+  else 
+    user = User.new(username: params[:username],password: params[:password])
+    user.save
+    redirect "/login" 
   end
   end
 
@@ -36,17 +37,17 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(name:params[:name])
+    user = User.find_by(username:params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      erb :account
+      redirect "/account"
     else
       redirect "/failure"
     end
   end
 
   get "/failure" do
-    erb :failure
+  erb :failure
   end
 
   get "/logout" do
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 20200604221122) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
